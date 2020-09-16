3d01f5aceac21214e1d4287269aa635ea59642d9
Anneta Wamono <annetawamono@yahoo.com.au>
2020-09-16 14:40:11 -0700

    feat: vendor metaboxes

--- includes/luv-functions.php
+++ includes/luv-functions.php
@@ -19,19 +19,38 @@ add_action( 'admin_menu', 'luv_Add_Admin_Link' );
  */
 
 function luv_Create_Vendor_Posttype() {
-  register_post_type( 'movies',
+  register_post_type( 'vendors',
     array(
       'labels' => array(
         'name' => __( 'Vendors' ),
-        'singular_name' => __( 'Movie' )
+        'singular_name' => __( 'Vendor' )
       ),
       'public' => true,
       'has_archive' => true,
       'rewrite' => array('slug' => 'vendors'),
+      'register_meta_box_cb' => 'luv_Create_Meta_Boxes',
     )
   );
 }
 add_action( 'init', 'luv_Create_Vendor_Posttype' );
+
+/*
+ * Create meta boxes for vendors
+ */
+
+function luv_Create_Meta_Boxes() {
+  //short_desc, special_note, phone, email, address, links
+  add_meta_box( 'luv_meta', 'Meta box title', 'luv_meta_callback', 'vendors' );
+}
+add_action( 'add_meta_boxes', 'luv_Create_Meta_Boxes' );
+
+/*
+ * Meta box callbacks
+ */
+
+function luv_meta_callback( $post ) {
+  echo 'meta hey';
+}
 
 /*
  * Create shortcode for displaying vendor details in alphabetized list
