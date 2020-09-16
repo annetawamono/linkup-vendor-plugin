9ade38032394e0ffbd4f0b93429182b9f3f9cf30
Anneta Wamono <annetawamono@yahoo.com.au>
2020-09-16 11:59:18 -0700

    Initial commit

---
+++ link-up-vendors.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Link Up Vendors
+Plugin URI: https://themefoundation.com/
+Description: A plugin to manage and display vendors on the Link Up website
+Author: Anneta Wamono
+Version: 1.0
+Author URI: https://annetawamono.github.io/portfolio/
+*/
+
+?>
